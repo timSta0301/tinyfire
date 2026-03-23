9bb39377c47f96542c6812a21e727b3a0d957617
thesebsterseb <sebastian.zimmermann@fau.de>
2026-03-23 16:27:49 +0100

    Added new images for testing. Image1 is a screenshot from the dataset

--- first_tests/YOLOv26/main.py
+++ first_tests/YOLOv26/main.py
@@ -6,7 +6,7 @@ _dir = os.path.dirname(os.path.abspath(__file__))
 model = YOLO(os.path.join(_dir, "best.pt"))
 
 # Run inference
-results = model.predict(os.path.join(_dir, "image.png"), conf=0.25)
+results = model.predict(os.path.join(_dir, "image5.png"), conf=0.25)
 
 # Process results
 for result in results:
